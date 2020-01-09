461d54bf3c167f477feed53a973388cbc2c004ef
NazarKY <KovalchukNY@gmail.com>
2020-01-09 14:02:40 +0200

    update for success deploy and add help info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
 
 import Graph from './graph';
 
@@ -8,11 +8,17 @@ import './app.scss';
 class App extends React.Component {
   render() {
     return (
-        <Router>
-          <div id='content'>
-            <Route exact={true} path="/" component={Graph} />
-          </div>
-        </Router>
+      <HashRouter basename='/'>
+        <div id='content'>
+          <ul>
+            <li>To add nodes, hold shift and click on the grid or press on button "Add Node".</li>
+            <li>To add edges, hold shift and click/drag to between nodes.</li>
+            <li>To delete a node or edge, click on it and press delete.</li>
+            <li>Click and drag nodes to change their position.</li>
+          </ul>
+          <Route exact path="/" component={Graph} />
+        </div>
+      </HashRouter>
     );
   }
 }
